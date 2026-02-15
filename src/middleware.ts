b66d08f9e8680fa6cce3795177cfb76090e501b6
maikdevries <dev.maik.devries@outlook.com>
2026-02-15 22:42:21 +0100

    fix: recombine composition to achieve complete type safety

--- src/middleware.ts
+++ src/middleware.ts
@@ -28,7 +28,7 @@ export type Handler<C = Empty> = (request: Request, context: C) => Response | Pr
 export type Middleware<R = Empty, P = Empty> = (request: Request, context: R, next: Handler<Merge<R, P>>) => Response | Promise<Response>;
 
 /**
- * A middleware chain that can be extended with {@link Middleware} and {@link Chain} or ended with {@link Handler}.
+ * A middleware chain that can be extended with {@link Middleware} and {@link Chain} or ended with a {@link Handler}.
  *
  * @template R - Context properties that this middleware chain requires
  * @template P - Context properties that this middleware chain provides
@@ -37,6 +37,16 @@ export type Middleware<R = Empty, P = Empty> = (request: Request, context: R, ne
  */
 type Chain<R, P> = {
 	(request: Request, context: R, next: Handler<Merge<R, P>>): Response | Promise<Response>;
+
+	/**
+	 * Appends a {@link Handler} to the current chain, ending the middleware chain.
+	 *
+	 * @template RS - Context properties that the appended handler requires
+	 *
+	 * @param handler - {@link Handler} to append
+	 * @returns Complete request handler with updated context object types
+	 */
+	add<RS>(handler: Handler<RS>): Handler<Reduce<R, RS, P>>;
 
 	/**
 	 * Appends a {@link Middleware} or {@link Chain} to the current chain, extending the context type.
@@ -48,63 +58,44 @@ type Chain<R, P> = {
 	 * @returns Middleware chain with updated context object types
 	 */
 	add<RS, PS>(next: Middleware<RS, PS> | Chain<RS, PS>): Chain<Reduce<R, RS, P>, Merge<P, PS>>;
-
-	/**
-	 * Appends a {@link Handler} to the current chain, ending the middleware chain.
-	 *
-	 * @template RS - Context properties that the appended handler requires
-	 *
-	 * @param handler - {@link Handler} to append
-	 * @returns Complete request handler with updated context object types
-	 */
-	end<RS>(handler: Handler<RS>): Handler<Reduce<R, RS, P>>;
 };
 
 /**
- * Composes two {@link Middleware} or a {@link Middleware} and a {@link Chain} into a single {@link Middleware}.
+ * Composes two {@link Middleware} or one {@link Middleware} and one {@link Chain} or {@link Handler} into a single {@link Middleware}.
  *
  * The first middleware receives the original context, the second receives the context modified by the first.
  *
  * @template RF - Context properties that the first middleware requires
  * @template PF - Context properties that the first middleware provides
- * @template RS - Context properties that the second middleware or chain requires
+ * @template RS - Context properties that the second middleware or chain or handler requires
  * @template PS - Context properties that the second middleware or chain provides
  *
  * @param first - First {@link Middleware}
- * @param second - Second {@link Middleware} or {@link Chain}
+ * @param second - Second {@link Middleware} or {@link Chain} or {@link Handler}
  * @returns Composed {@link Middleware} with updated context object types
  */
-function composeMiddleware<RF, PF, RS, PS>(
+function compose<RF, PF, RS, PS>(
 	first: Middleware<RF, PF>,
-	second: Middleware<RS, PS> | Chain<RS, PS>,
+	second: Middleware<RS, PS> | Chain<RS, PS> | Handler<RS>,
 ): Middleware<Reduce<RF, RS, PF>, Merge<PF, PS>> {
 	return (request, context, next) => first(request, context, (r, c) => second(r, c as RS, next as unknown as Handler<Merge<RS, PS>>));
 }
 
 /**
- * Composes a {@link Middleware} and a {@link Handler} into a single {@link Handler}.
+ * This version should not be used in practice as it simply returns an unchanged copy of the input handler. This is included only to
+ * achieve complete type safety.
  *
- * The middleware receives the original context, the handler receives the context modified by the middleware.
- *
- * @template RF - Context properties that the middleware requires
- * @template PF - Context properties that the middleware provides
- * @template RS - Context properties that the handler requires
- *
- * @param middleware - The {@link Middleware}
- * @param handler - The {@link Handler}
- * @returns Composed {@link Handler} with updated context object types
+ * @ignore
  */
-function composeHandler<RF, PF, RS>(middleware: Middleware<RF, PF>, handler: Handler<RS>): Handler<Reduce<RF, RS, PF>> {
-	return (request, context) => middleware(request, context, (r, c) => handler(r, c as RS));
-}
+export function chain<R>(handler: Handler<R>): Handler<R>;
 
 /**
- * Creates a middleware chain that allows composing (multiple) {@link Middleware} and a {@link Handler}.
+ * Creates a middleware chain that allows composing (multiple) {@link Middleware} with a {@link Handler}.
  *
  * @template R - Context properties that the middleware requires
  * @template P - Context properties that the middleware provides
  *
- * @param middleware - {@link Middleware} to make chainable
+ * @param middleware - {@link Middleware} to make composable
  * @returns Middleware chain
  *
  * @example Basic usage
@@ -113,14 +104,17 @@ function composeHandler<RF, PF, RS>(middleware: Middleware<RF, PF>, handler: Han
  * const authorise: Middleware<Empty, { user: User }> = ...;
  * const handler: Handler<{ id: string, user: User }> = ...;
  *
- * const app = chain(logger).add(authorise).end(handler);
+ * const app = chain(logger).add(authorise).add(handler);
  * ```
  */
-export function chain<R, P>(middleware: Middleware<R, P>): Chain<R, P> {
-	const copy = middleware.bind(null) as Chain<R, P>;
+export function chain<R, P>(middleware: Middleware<R, P>): Chain<R, P>;
 
-	copy.add = <RS, PS>(next: Middleware<RS, PS> | Chain<RS, PS>) => chain(composeMiddleware(middleware, next));
-	copy.end = <RS>(handler: Handler<RS>) => composeHandler(middleware, handler);
+// @ts-ignore: https://github.com/denoland/deno/issues/30285
+export function chain<R, P>(initial: Handler<R> | Middleware<R, P>): Handler<R> | Chain<R, P> {
+	const copy = initial.bind(null);
 
-	return copy;
+	// @ts-expect-error: The function type is defined as part of the Chain type which prevents exposure on Handler
+	copy.add = <RS, PS>(next: Middleware<RS, PS> | Chain<RS, PS>) => chain(compose(initial, next));
+
+	return copy as Handler<R> | Chain<R, P>;
 }
