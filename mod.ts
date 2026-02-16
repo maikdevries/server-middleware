ed3634ef27f7ac41426cd7af18b9f60e75ddba31
maikdevries <dev.maik.devries@outlook.com>
2026-02-16 21:54:03 +0100

    docs: write documentation for module

--- mod.ts
+++ mod.ts
@@ -1,3 +1,32 @@
+/**
+ * This module provides a flexible approach to define and compose contextful middleware functions in a type-safe manner. Each middleware
+ * can specify both the context properties it requires and the properties it provides. A final request handler consumes the accumulated
+ * context and produces a response. The type system ensures at compile-time that all required properties are provided by earlier middleware
+ * in the chain.
+ *
+ * @example Basic usage
+ * ```ts
+ * import type { Handler, Middleware } from '@maikdevries/server-middleware';
+ * import { chain } from '@maikdevries/server-middleware';
+ *
+ * const uuid: Middleware<Empty, { uuid: string }> = async (request, context, next) => {
+ * 	return await next(request, { ...context, 'uuid': self.crypto.randomUUID() });
+ * };
+ *
+ * const authorise: Middleware<{ uuid: string }, { user: User }> = async (request, context, next) => {
+ * 	return await next(request, { ...context, 'user': authoriseUser(context.uuid) });
+ * };
+ *
+ * const greeting: Handler<{ user: User }> = async (request, context) => {
+ * 	return new Response(`Hi ${context.user.name}!`);
+ * };
+ *
+ * const app = chain(uuid).add(authorise).add(greeting);
+ * ```
+ *
+ * @module
+ */
+
 export type { Handler, Middleware } from './src/middleware.ts';
 export { chain } from './src/middleware.ts';
 
