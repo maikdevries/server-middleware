b3fb2714219f8139be5e5766c00527632da2e355
maikdevries <dev.maik.devries@outlook.com>
2026-02-18 23:07:16 +0100

    feat: introduce wide-event logging middleware

---
+++ src/middleware/logger.ts
@@ -0,0 +1,42 @@
+import type { Empty, Middleware } from '@maikdevries/server-middleware';
+
+export interface Log {
+	'error'?: {
+		'reason': string;
+		'type': string;
+	};
+	'id': string;
+	'request': {
+		'method': Request['method'];
+		'url': URL['href'];
+	};
+	'response'?: {
+		'status': Response['status'];
+	};
+	'timestamp': number;
+}
+
+export function logger(): Middleware<Empty, { 'log': Log }> {
+	return async (request, context, next) => {
+		const log: Log = {
+			'id': self.crypto.randomUUID(),
+			'request': {
+				'method': request.method,
+				'url': request.url,
+			},
+			'timestamp': self.Date.now(),
+		};
+
+		try {
+			const response = await next(request, { ...context, 'log': log });
+
+			log.response = {
+				'status': response.status,
+			};
+
+			return response;
+		} finally {
+			console.log(log);
+		}
+	};
+}
